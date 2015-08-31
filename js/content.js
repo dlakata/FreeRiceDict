800ac94aa1b6ca527cd8b4b4909e574161482772
David Lakata <davidlakata@gmail.com>
2015-08-31 10:21:05 -0400

    fixes bug for empty string

--- js/content.js
+++ js/content.js
@@ -1,9 +1,15 @@
 $(window).bind('hashchange', function (e) {
+    if (window.location.hash.indexOf("english-vocabulary") < 0) {
+        return;
+    }
+    var definitions = $('#game-status');
+    var word = definitions.text().trim().split(/ +/)[1];
+    if (!word) {
+        return;
+    }
     var api_base = 'http://api.wordnik.com/v4/word.json/';
     var api_key = 'API_KEY';
     var api_options = '/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=' + api_key;
-    var definitions = $('#game-status');
-    var word = definitions.text().trim().split(/ +/)[1];
     $.getJSON(api_base + word + api_options, function (data) {
         var text = '<br>';
         $(data).each(function (i, def) {
